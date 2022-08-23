b0a22a27aa2ff286e7883d1edb861a3f9c6bb1f5
Ramazan USLU <ruslu1720@gmail.com>
2022-08-23 21:28:12 +0300

    s-2 6-Degisken turleri

---
+++ Section-2-JavaScript-Temelleri/6-Degisken-Turleri/script.js
@@ -0,0 +1,34 @@
+var urunAdi = "Iphone 13";
+let urunFiyat = 15000;
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+let sayi1 = "10";
+let sayi2 = "20";
+
+console.log(sayi1 + sayi2);
+
+let sayi3 = 10;
+let sayi4 = 20;
+
+console.log(sayi3 + sayi4);
+
+let sayi5 = "10";
+let sayi6 = "20";
+
+console.log(Number(sayi5) + Number(sayi6));
+
+let sayi7 = 10;
+let sayi8 = 20;
+
+console.log(sayi7.toString() + sayi8.toString());
+
+let isim = "React";
+let soyisim = "Developer";
+
+console.log(isim + " " + soyisim);
+
+let sinavNotu = 70;
+let basarilimi = sinavNotu >= 50;
+console.log(basarilimi);

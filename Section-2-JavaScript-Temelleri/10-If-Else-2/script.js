c63f7403ce04195cb10616e36ac11205dc1370d8
Ramazan USLU <ruslu1720@gmail.com>
2022-08-24 16:37:13 +0300

    S-2 10-if/else kosul ifadeleri - 2

---
+++ Section-2-JavaScript-Temelleri/10-If-Else-2/script.js
@@ -0,0 +1,11 @@
+// Yas >= 18
+// Mezuniyet == "lise" ya da mezuniyet == "üniversite"
+
+let yas = 20;
+let mezuniyet = "lise";
+
+if ((yas >= 18 && mezuniyet == "lise") || mezuniyet == "üniversite") {
+  console.log("Ehliyet Alabilir.");
+} else {
+  console.log("Ehliyet Alamaz.");
+}

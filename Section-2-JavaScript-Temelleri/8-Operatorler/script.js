9b0831870c727dfbaa6105839deb55b1355fc12e
Ramazan USLU <ruslu1720@gmail.com>
2022-08-24 15:48:09 +0300

    S-2 8-Opreatorler & README.md dosyasi eklendi

---
+++ Section-2-JavaScript-Temelleri/8-Operatorler/script.js
@@ -0,0 +1,35 @@
+let sonuc;
+let a = 10,
+  b = 20,
+  c = 30;
+//---------------------------------------------------------
+// 1-Aritmetik Operatörler
+
+// sonuc = a + b;
+// sonuc = a - b;
+// sonuc = a * b;
+// sonuc = a / b;
+// sonuc = c % a;
+// sonuc = a++;
+// sonuc = a--;
+// sonuc = ++a;
+// sonuc = --a;
+
+//---------------------------------------------------------
+// 2-Atama Operatörleri
+// sonuc = a;
+// sonuc += a;
+//---------------------------------------------------------
+// 3-Karşılatırma Operatörler
+sonuc = a == b;
+sonuc = a != b;
+sonuc = 3 === "3";
+sonuc = a < b;
+sonuc = a > b;
+sonuc = a <= b;
+//---------------------------------------------------------
+// 4-Mantıksal Operatörler
+
+
+//---------------------------------------------------------
+console.log(sonuc);

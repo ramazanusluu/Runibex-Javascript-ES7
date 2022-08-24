93f19aa48f8699fcba9ccaaad4662e7397e1ab34
Ramazan USLU <ruslu1720@gmail.com>
2022-08-24 16:26:32 +0300

    S-2 9-if/else kosul ifadeleri - 1

---
+++ Section-2-JavaScript-Temelleri/9-If-Else-1/script.js
@@ -0,0 +1,36 @@
+// if (3 > 2) {
+//     console.log("React Developer");
+// }
+// console.log("React- Next.JS");
+
+let userName = "reactDeveloper";
+let password = "1234";
+let isLoggedin = userName == "reactDeveloper";
+
+// if (isLoggedin) {
+//   console.log("Giriş işlemi başarılı");
+// }
+
+// if (!isLoggedin) {
+//   console.log("Giriş işlemi başarısız");
+// }
+
+// if (isLoggedin) {
+//   console.log("React developer");
+// } else {
+//   console.log("Next.js");
+// }
+
+// if (isLoggedin) {
+//   console.log("Giriş ilemi başarılı");
+// } else {
+//   console.log("Giriş işlemi başarısız");
+// }
+
+if ((userName = "reactDeveloper")) {
+  if ((password = "1234")) {
+    console.log("Giriş ilemi başarılı");
+  }
+} else {
+  console.log("Giriş işlemi başarısız");
+}

2ff001cb37601cc6f31b7e85dfd7ba2509103580
Ramazan USLU <ruslu1720@gmail.com>
2022-08-29 23:10:40 +0300

    S-5 47-Temel validation kurallarinin uygulanmasi - Required Validation

---
+++ Section-5-Proje-2-Form-Validator/script.js
@@ -0,0 +1,49 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const repassword = document.getElementById("repassword");
+
+function error(input, message) {
+  input.className = "form-control is-invalid";
+  const div = input.nextElementSibling;
+  div.innerText = message;
+  div.className = "invalid-feedback";
+}
+function success(input) {
+  input.className = "form-control is-valid";
+}
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  if (username.value === "") {
+    error(username, "Username zorunlu alan");
+  } else {
+    success(username);
+  }
+  if (email.value === "") {
+    error(email, "Email zorunlu alan");
+  } else if (!validateEmail(email.value)) {
+    error(email, "Email adresi hatalı");
+  } else {
+    success(email);
+  }
+  if (password.value === "") {
+    error(password, "Password zorunlu alan");
+  } else {
+    success(password);
+  }
+  if (repassword.value === "") {
+    error(repassword, "Re-Password zorunlu alan");
+  } else {
+    success(repassword);
+  }
+});

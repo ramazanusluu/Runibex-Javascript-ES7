f1e69480d4bbd6dff5ea37f4e3d42fa9e3372537
Ramazan USLU <ruslu1720@gmail.com>
2022-08-30 23:12:57 +0300

    S-10 75-Cevap seceneklerin kontrolu

--- section-10-Proje-6-Quiz-Uygulamasi/script.js
+++ section-10-Proje-6-Quiz-Uygulamasi/script.js
@@ -62,16 +62,23 @@ const quiz = new Quiz(sorular);
 document.querySelector(".btn-start").addEventListener("click", function () {
   document.querySelector(".quiz-box").classList.add("active");
   soruGoster(quiz.soruGetir());
+  document.querySelector(".next-btn").classList.remove("show");
 });
 
 document.querySelector(".next-btn").addEventListener("click", function () {
   if (quiz.sorular.length != quiz.soruIndex + 1) {
     quiz.soruIndex += 1;
     soruGoster(quiz.soruGetir());
+    document.querySelector(".next-btn").classList.remove("show");
   } else {
     console.log("Quiz Bitti");
   }
 });
+
+const option_list = document.querySelector(".option_list");
+const correctIcon = '<div class="icon"><i class="fa-solid fa-check"></i></div>';
+const inCorrectIcon =
+  '<div class="icon"><i class="fa-solid fa-xmark"></i></div>';
 
 function soruGoster(soru) {
   let question = `<span>${soru.soruMetni}</span>`;
@@ -84,5 +91,25 @@ function soruGoster(soru) {
     `;
   }
   document.querySelector(".question_text").innerHTML = question;
-  document.querySelector(".option_list").innerHTML = options;
+
+  option_list.innerHTML = options;
+  const option = option_list.querySelectorAll(".option");
+  for (let opt of option) {
+    opt.setAttribute("onclick", "optionSelected(this)");
+  }
+}
+function optionSelected(option) {
+  let cevap = option.querySelector("span b").textContent;
+  let soru = quiz.soruGetir();
+  if (soru.cevabıKontrolEt(cevap)) {
+    option.classList.add("correct");
+    option.insertAdjacentHTML("beforeend", correctIcon);
+  } else {
+    option.classList.add("incorrect");
+    option.insertAdjacentHTML("beforeend", inCorrectIcon);
+  }
+  for (let i = 0; i < option_list.children.length; i++) {
+    option_list.children[i].classList.add("disabled");
+  }
+  document.querySelector(".next-btn").classList.add("show");
 }

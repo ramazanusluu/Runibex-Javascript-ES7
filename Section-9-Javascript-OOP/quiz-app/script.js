d11e33ef09781e31d5d14662d92aa19a7ff4f9b8
Ramazan USLU <ruslu1720@gmail.com>
2022-08-30 15:10:49 +0300

    S-9 69-Nesne olusturma: Contructor

--- Section-9-Javascript-OOP/quiz-app/script.js
+++ Section-9-Javascript-OOP/quiz-app/script.js
@@ -1,36 +1,99 @@
 // OOP: Nesne Tabanlı Programlama
 //Object
 
-let soru = {
-  soruMetni: "Hangisi javascript paket yönetim uygulamasıdır",
-  cevapSecenekleri: {
+// let soru = {
+//   soruMetni: "Hangisi javascript paket yönetim uygulamasıdır",
+//   cevapSecenekleri: {
+//     a: "Node.js",
+//     b: "Typescript",
+//     c: "Npm",
+//   },
+//   dogruCevap: "c",
+//   cevabıKontrolEt: function (cevap) {
+//     return cevap === this.dogruCevap;
+//   },
+// };
+
+// let soru2 = {
+//   soruMetni: "Hangisi .net paket yönetim uygulamasıdır",
+//   cevapSecenekleri: {
+//     a: "Node.js",
+//     b: "nuget",
+//     c: "Npm",
+//   },
+//   dogruCevap: "b",
+//   cevabıKontrolEt: function (cevap) {
+//     return cevap === this.dogruCevap;
+//   },
+// };
+
+// console.log(soru.soruMetni);
+// console.log(soru.cevabıKontrolEt("c"));
+
+// Sınıf, Constructor => nesne * 30
+// ES5, ES6, ES7
+
+//-------------------------------------------------------------------
+
+function Soru(soruMetni, cevapSecenekleri, dogruCevap) {
+  this.soruMetni = soruMetni;
+  this.cevapSecenekleri = cevapSecenekleri;
+  this.dogruCevap = dogruCevap;
+  this.cevabıKontrolEt = function (cevap) {
+    return cevap === this.dogruCevap;
+  };
+}
+
+let soru1 = new Soru(
+  "Hangisi javascript paket yönetim uygulamasıdır",
+  {
     a: "Node.js",
     b: "Typescript",
     c: "Npm",
   },
-  dogruCevap: "c",
-  cevabıKontrolEt: function (cevap) {
-    return cevap === this.dogruCevap;
-  },
-};
-
-let soru2 = {
-  soruMetni: "Hangisi .net paket yönetim uygulamasıdır",
-  cevapSecenekleri: {
+  "c"
+);
+let soru2 = new Soru(
+  "Hangisi .net paket yönetim uygulamasıdır",
+  {
     a: "Node.js",
     b: "nuget",
     c: "Npm",
   },
-  dogruCevap: "b",
-  cevabıKontrolEt: function (cevap) {
-    return cevap === this.dogruCevap;
-  },
-};
+  "b"
+);
 
-console.log(soru.soruMetni);
-console.log(soru.cevabıKontrolEt("c"));
-console.log(soru2.soruMetni);
-console.log(soru2.cevabıKontrolEt("c"));
+console.log(soru1.soruMetni);
+console.log(soru1.dogruCevap);
 
-// Sınıf, Constructor => nesne * 30
-// ES5, ES6, ES7
+console.log(soru2.doruMetni);
+console.log(soru2.dogruCevap);
+console.log(soru1.cevabıKontrolEt("c"));
+
+//-------------------------------------------------------------------
+
+let sorular = [
+  new Soru(
+    "Hangisi javascript paket yönetim uygulamasıdır",
+    {
+      a: "Node.js",
+      b: "Typescript",
+      c: "Npm",
+    },
+    "c"
+  ),
+  new Soru(
+    "Hangisi javascript paket yönetim uygulamasıdır",
+    {
+      a: "Node.js",
+      b: "Typescript",
+      c: "Npm",
+    },
+    "c"
+  ),
+];
+
+console.log(sorular[0]);
+for (let s of sorular) {
+  console.log(s.soruMetni);
+}

f2dd45461c9f04a082d7f913186e584d9a404097
Ramazan USLU <ruslu1720@gmail.com>
2022-09-01 18:48:49 +0300

    S-12 93-Arayuzun olusturulmasi(ui)

--- Section-12-Proje-7-Kurs-Uygulamasi/AppES5.js
+++ Section-12-Proje-7-Kurs-Uygulamasi/AppES5.js
@@ -1,8 +1,35 @@
+//  Course Constructor
 function Course(title, instructor, image) {
   this.title = title;
   this.instructor = instructor;
   this.image = image;
 }
+
+//  UI Constructor
+function UI() {}
+
+UI.prototype.addCourseToList = function (course) {
+  const list = document.getElementById("course-list");
+  let html = `
+    <tr>
+        <td>
+            <img src="img/${course.image}" width="50px"/>
+        </td>
+        <td>${course.title}</td>
+        <td>${course.instructor}</td>
+        <td>
+            <a href="#" class="btn btn-danger btn-slm">Delete</a>
+        </td>
+    </tr>
+  `;
+  list.innerHTML += html;
+};
+
+UI.prototype.clearControls = function () {
+  const title = (document.getElementById("title").value = "");
+  const instructor = (document.getElementById("instructor").value = "");
+  const image = (document.getElementById("image").value = "");
+};
 
 document.getElementById("new-course").addEventListener("submit", function (e) {
   const title = document.getElementById("title").value;
@@ -13,10 +40,14 @@ document.getElementById("new-course").addEventListener("submit", function (e) {
   //   create course object
   const course = new Course(title, instructor, image);
 
-  console.log(course);
-  //   save to database
+  //   create UI
+  const ui = new UI();
 
-  //   show on the ui
+  //   add course to list
+  ui.addCourseToList(course);
+
+  //   clear control
+  ui.clearControls();
 
   e.preventDefault();
 });

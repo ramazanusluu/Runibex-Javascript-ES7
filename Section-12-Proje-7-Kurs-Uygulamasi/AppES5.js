6ae3cdfdef72696d9a7e9b599530c0ff99862ae4
Ramazan USLU <ruslu1720@gmail.com>
2022-09-01 19:04:08 +0300

    S-12 93-Listeden Eleman Silme

--- Section-12-Proje-7-Kurs-Uygulamasi/AppES5.js
+++ Section-12-Proje-7-Kurs-Uygulamasi/AppES5.js
@@ -18,7 +18,7 @@ UI.prototype.addCourseToList = function (course) {
         <td>${course.title}</td>
         <td>${course.instructor}</td>
         <td>
-            <a href="#" class="btn btn-danger btn-slm">Delete</a>
+            <a href="#" class="btn btn-danger btn-sm delete">Delete</a>
         </td>
     </tr>
   `;
@@ -29,6 +29,12 @@ UI.prototype.clearControls = function () {
   const title = (document.getElementById("title").value = "");
   const instructor = (document.getElementById("instructor").value = "");
   const image = (document.getElementById("image").value = "");
+};
+
+UI.prototype.deleteCourse = function (element) {
+  if (element.classList.contains("delete")) {
+    element.parentElement.parentElement.remove();
+  }
 };
 
 document.getElementById("new-course").addEventListener("submit", function (e) {
@@ -50,4 +56,9 @@ document.getElementById("new-course").addEventListener("submit", function (e) {
   ui.clearControls();
 
   e.preventDefault();
+});
+
+document.getElementById("course-list").addEventListener("click", function (e) {
+  const ui = new UI();
+  ui.deleteCourse(e.target);
 });

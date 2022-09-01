9a676131c6f7f7ccd0ba260e23a5383a9bd9e9c2
Ramazan USLU <ruslu1720@gmail.com>
2022-09-01 18:27:07 +0300

    S-12 91-Course Objesinin Olusturulmasi

---
+++ Section-12-Proje-7-Kurs-Uygulamasi/AppES5.js
@@ -0,0 +1,22 @@
+function Course(title, instructor, image) {
+  this.title = title;
+  this.instructor = instructor;
+  this.image = image;
+}
+
+document.getElementById("new-course").addEventListener("submit", function (e) {
+  const title = document.getElementById("title").value;
+  const instructor = document.getElementById("instructor").value;
+  const image = document.getElementById("image").value;
+  //   console.log(title, instructor, image);
+  //---------------------------------------------------------
+  //   create course object
+  const course = new Course(title, instructor, image);
+
+  console.log(course);
+  //   save to database
+
+  //   show on the ui
+
+  e.preventDefault();
+});

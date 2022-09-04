849f4cfe7e5f03df35210cd99078c211de49a2df
Ramazan USLU <ruslu1720@gmail.com>
2022-09-04 18:53:41 +0300

    S-15 125-Döviz kurlarinin hesaplanmasi

--- Section-15-Proje-9-Doviz-Ceviri-Uygulamasi/App.js
+++ Section-15-Proje-9-Doviz-Ceviri-Uygulamasi/App.js
@@ -17,7 +17,7 @@ fetch(url + "/codes")
     const items = data.supported_codes;
     let options;
     for (let item of items) {
-      console.log(item);
+      // console.log(item);
       options += `
         <option value=${item[0]}>${item[1]}</option>
       `;
@@ -25,3 +25,23 @@ fetch(url + "/codes")
     list_one.innerHTML = options;
     list_two.innerHTML = options;
   });
+calculate.addEventListener("click", function () {
+  const doviz1 = currency_one.value;
+  const doviz2 = currency_two.value;
+  const miktar = amount.value;
+  console.log(doviz1, doviz2, miktar);
+  fetch(url + "/latest/" + doviz1)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      console.log(data.conversion_rates[doviz2] * miktar);
+      const sonuc = (data.conversion_rates[doviz2] * miktar).toFixed(3);
+      result.innerHTML = `
+        <div class="card border-primary">
+          <div class="card-body text-center" style="font-size: 30px">
+            ${miktar} ${doviz1} =  ${sonuc} ${doviz2}
+          </div>
+        </div>
+      `;
+    });
+});

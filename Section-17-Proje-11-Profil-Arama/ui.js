6d265932bc37853a9129e6c28cdbd6420cbd2893
Ramazan USLU <ruslu1720@gmail.com>
2022-09-05 09:59:15 +0300

    S-17 135-Profil bilgilerinin tasarima aktarilmasi

---
+++ Section-17-Proje-11-Profil-Arama/ui.js
@@ -0,0 +1,52 @@
+class UI {
+  constructor() {
+    this.profileContainer = document.querySelector("#profileContainer");
+    this.alert = document.querySelector("#alert");
+  }
+  showProfile(profile) {
+    this.profileContainer.innerHTML = `
+        <div class="card card-body">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="https://dummyimage.com/300x300/fff/aaa" class="img-thumbnail" alt="resim" />
+                </div>
+                <div class="col-md-9">
+                    <h4>Contact</h4>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                           name: ${profile.name}
+                        </li>
+                        <li class="list-group-item">
+                           username: ${profile.username}
+                        </li>
+                        <li class="list-group-item">
+                           email: ${profile.email}
+                        </li>
+                        <li class="list-group-item">
+                           address: ${profile.address.street} - ${profile.address.city} - ${profile.address.zipcode} - ${profile.address.suite}
+                        </li>
+                        <li class="list-group-item">
+                           phone: ${profile.phone}
+                        </li>
+                        <li class="list-group-item">
+                           website: ${profile.website}
+                        </li>
+                        <li class="list-group-item">
+                           company: ${profile.company.name}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `;
+  }
+
+  showAlert(text) {
+    this.alert.innerHTML = `${text} is not found.`;
+  }
+
+  clear() {
+    this.profileContainer.innerHTML = "";
+    this.alert.innerHTML = "";
+  }
+}
